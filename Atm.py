92d915a3eb260221d565ad3f6cfe57cd85fa5ab8
ranichettri <71009937+ranichettri@users.noreply.github.com>
2022-03-22 18:39:26 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,14 @@
+class Atm:
+    def __init__(self,cardNumber,pinNumber):
+        self.card = cardNumber
+        self.pin = pinNumber
+
+    def CashWithdrawl(self):
+        print("Your cash is withdrawed in ****atm")
+
+    def BalanceEnquiryl(self):
+        print("Your balance is ******!!! ENJOY THE WEEKEND:)!")
+
+    def CashDeposite(self):
+        print("Your cash is deposited successful !!!")
+
